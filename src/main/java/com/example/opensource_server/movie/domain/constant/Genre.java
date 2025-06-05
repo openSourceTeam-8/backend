12ac080bd8b799c18736e61cfab6cb5b4b2bb76b
10Hyuna <kimhaa1005@naver.com>
2025-06-05 21:14:25 +0900

    FEAT: 장르를 담아 둘 ENUM 클래스 정의

--- src/main/java/com/example/opensource_server/movie/domain/constant/Genre.java
+++ src/main/java/com/example/opensource_server/movie/domain/constant/Genre.java
@@ -1,4 +1,28 @@
 package com.example.opensource_server.movie.domain.constant;
 
 public enum Genre {
+    ACTION("액션"),
+    COMEDY("코미디"),
+    ROMANCE("로맨스"),
+    THRILLER("스릴러"),
+    HORROR("공포"),
+    DRAMA("드라마"),
+    SF("SF"),
+    ANIMATION("애니메이션"),
+    CRIME("범죄"),
+    WAR("전쟁"),
+    FANTASY("판타지"),
+    MUSICAL("음악(뮤지컬)"),
+    DOCUMENTARY("다큐멘터리"),
+    SPORTS("스포츠");
+
+    private final String displayName;
+
+    Genre(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
