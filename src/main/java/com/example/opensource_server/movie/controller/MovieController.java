75b207e98b1bd95f95f376eee12352f254421a35
10Hyuna <kimhaa1005@naver.com>
2025-06-05 21:18:27 +0900

    FIX: 장르별 영화 조회 api route 표기 더 명확하게 수정

--- src/main/java/com/example/opensource_server/movie/controller/MovieController.java
+++ src/main/java/com/example/opensource_server/movie/controller/MovieController.java
@@ -1,13 +1,19 @@
 package com.example.opensource_server.movie.controller;
 
+import com.example.opensource_server.movie.dto.request.MovieByGenreRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/movie")
+@RequestMapping("/movies")
 @Tag(name = "Movie API", description = "영화 관련 API")
 public class MovieController {
+
 }
