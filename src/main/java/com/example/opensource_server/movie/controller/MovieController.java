398a73a12545455848595a09c8a6c3723a94b76a
10Hyuna <kimhaa1005@naver.com>
2025-06-05 21:16:42 +0900

    FEAT: 컨트롤러 스웨거 config

--- src/main/java/com/example/opensource_server/movie/controller/MovieController.java
+++ src/main/java/com/example/opensource_server/movie/controller/MovieController.java
@@ -1,4 +1,13 @@
 package com.example.opensource_server.movie.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/movie")
+@Tag(name = "Movie API", description = "영화 관련 API")
 public class MovieController {
 }
