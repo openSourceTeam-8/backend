0d21f1f0271a6feee4c7b301e2fbd0ddd05577d9
10Hyuna <kimhaa1005@naver.com>
2025-06-05 21:24:27 +0900

    FEAT: 반환할 때 반환 내용을 담아 둘 추상적인 반환 dto 선언

---
+++ src/main/java/com/example/opensource_server/config/dto/ResponseFrom.java
@@ -0,0 +1,21 @@
+package com.example.opensource_server.config.dto;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class ResponseFrom {
+
+    public ResponseFrom() {
+        response = new HashMap<>();
+    }
+
+    public void of(String value1, Object value2) {
+        response.put(value1, value2);
+    }
+
+    private Map<String, Object> response;
+
+}
