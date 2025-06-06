e5e8bd560592a9e20968a5cab9bc29ce441a62dc
10Hyuna <kimhaa1005@naver.com>
2025-06-06 14:05:55 +0900

    FIX: 스웨거 로그인 없이 접근 가능하도록 설정 수정

--- src/main/java/com/example/opensource_server/common/config/WebConfig.java
+++ src/main/java/com/example/opensource_server/common/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
     }
